5e37505ea50fc931fccd2bf5353da6cd3d1b359e
kacky24 <kakiuchi@crimson.q.t.u-tokyo.ac.jp>
2020-02-10 03:35:48 +0900

    scripts to run training on mnist

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "app",
+    products: [
+        .library(name: "Datasets", targets: ["Datasets"]),
+        .library(name: "ModelSupport", targets: ["ModelSupport"]),
+        .executable(name: "LeNet-MNIST", targets: ["LeNet-MNIST"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "Datasets", dependencies: ["ModelSupport"], path: "Datasets"),
+        .target(name: "ModelSupport", path: "Support"),
+        .target(
+            name: "LeNet-MNIST", dependencies: ["Datasets"],
+            path: "Examples/LeNet-MNIST"),
+    ]
+)
